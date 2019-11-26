fa08bcf6254146d77591bc55c736487e6e46b57e
David Andersson <david.andersson@zocom.se>
2019-11-26 10:40:58 +0100

    Final version

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const data = [
 	{
-		name: 'cat',
+		name: 'Cat',
 		cuddliness: 5,
 		independence: 4,
 		likability: 4
 	},
 	{
-		name: 'crocodile',
+		name: 'Crocodile',
 		cuddliness: 1,
 		independence: 5,
 		likability: 2
@@ -16,6 +16,12 @@ const data = [
 		cuddliness: 1,
 		independence: 3,
 		likability: 4
+	},
+	{
+		name: 'Dog',
+		cuddliness: 5,
+		independence: 2,
+		likability: 4
 	}
 ]
 
@@ -24,28 +30,38 @@ const data = [
 // lägga till varje card i DOM
 
 console.log('Hello!');
-let petcards = document.querySelector('.petcards');
+window.addEventListener('load', () => {
+	let petcards = document.querySelector('.petcards');
 
-for(let i=0; i<data.length; i++) {
-	let card = document.createElement('div');
-	card.classList.add('card');
-	// card.className = 'card';  <- gör samma sak som classList men med mindre precision
-	card.innerText = 'CARD TEST';
-	console.log('inuti for-loopen', card);
+	// Optional: use higher-order functions instead of normal for loop (forEach or map)
+	for(let i=0; i<data.length; i++) {
+		let card = document.createElement('div');
+		card.classList.add('card');
+		// card.className = 'card';  <- gör samma sak som classList men med mindre precision
 
-	petcards.appendChild(card);
+		let title = document.createElement('div');
+		title.classList.add('title');
+		title.innerText = data[i].name;
+		card.appendChild(title);
+
+		card.appendChild(makePetRating('cuddliness', data[i].cuddliness));
+		card.appendChild(makePetRating('independence', data[i].independence));
+		card.appendChild(makePetRating('likability', data[i].likability));
+
+		petcards.appendChild(card);
+	}
+
+});  // window load
+
+function makePetRating(cssClass, value) {
+	let property = document.createElement('div');
+	property.classList.add(cssClass);
+	property.classList.add('score' + value);
+	let letter = cssClass.charAt(0).toUpperCase();  // alternative: substr
+	property.innerText = letter + value;
+	return property;
 }
 
-/*
-<div class="card">
-	<div class="title">
-		Tortoise
-	</div>
-	<div class="cuddliness">1 </div>
-	<div class="independence">3 </div>
-	<div class="likability">4 </div>
-</div>
-*/
 
 
 
